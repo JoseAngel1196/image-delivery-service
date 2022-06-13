2244b2590650368c005a2d69c6b9a33ad3faba3a
Jose Hidalgo <joseangel1196@hotmail.com>
2022-06-12 22:07:32 -0400

    final

--- infrastructure/compute/main.tf
+++ infrastructure/compute/main.tf
@@ -235,74 +235,74 @@ resource "aws_autoscaling_group" "autoscaling-group-front-end-server-image-deliv
 ###### CloudFront Distribution ######
 #####################################
 
-# resource "aws_cloudfront_distribution" "cloudfront-image-delivery-service" {
-#   origin {
-#     domain_name = aws_alb.alb-image-delivery-service.dns_name
-#     origin_id   = aws_alb.alb-image-delivery-service.id
+resource "aws_cloudfront_distribution" "cloudfront-image-delivery-service" {
+  origin {
+    domain_name = aws_alb.alb-image-delivery-service.dns_name
+    origin_id   = aws_alb.alb-image-delivery-service.id
 
-#     custom_origin_config {
-#       origin_protocol_policy = "http-only"
-#       http_port = "80"
-#       https_port = "443"
-#       origin_ssl_protocols = ["TLSv1"]
-#     }
-#   }
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
 
-#   enabled             = true
+  enabled             = true
 
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = aws_alb.alb-image-delivery-service.id
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_alb.alb-image-delivery-service.id
 
-#     forwarded_values {
-#       query_string = true
+    forwarded_values {
+      query_string = true
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
 
-#   # Cache behavior with precedence 1
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/immutable/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = aws_alb.alb-image-delivery-service.id
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_alb.alb-image-delivery-service.id
 
-#     forwarded_values {
-#       query_string = true
+    forwarded_values {
+      query_string = true
 
-#       cookies {
-#         forward = "none"
-#       }
-#     }
+      cookies {
+        forward = "none"
+      }
+    }
 
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true
-#     viewer_protocol_policy = "allow-all"
-#   }
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "allow-all"
+  }
 
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "whitelist"
-#       locations        = ["US", "CA", "GB", "DE"]
-#     }
-#   }
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
 
-#   tags = {
-#     Environment = "production"
-#   }
+  tags = {
+    Environment = "production"
+  }
 
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
